f2d8f21394792dfa88a3e91448e050a0b138c3ca
solarjoni <>
2021-05-28 20:43:38 +0000

    Use body-parser to Parse POST Requests

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.use(function middleware(req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
